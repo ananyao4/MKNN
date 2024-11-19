61d7cc577329be232b18b87b5d006ecd296054f3
ananyao4 <155084958+ananyao4@users.noreply.github.com>
2024-11-19 14:50:34 +0700

    Update home.py

--- home.py
+++ home.py
@@ -1,7 +1,12 @@
 import streamlit as st
 import pandas as pd 
 
-st.title("Website Developing using Python")
-st.header("Website Developing using Python")
+st.title("🎇🎆Website Developing using Python🎆🎇")
+st.header("🎈Website Developing using Python✨")
 
-st.subheader("Ananya Khiaohwan")
+st.image('./img/รูปป.jpg')
+st.subheader("❤Ananya Khiaohwan")
+
+dt=pd.read_csv('./data/iris-3.csv')
+st.header()
+st.erite(dt.head(10))
