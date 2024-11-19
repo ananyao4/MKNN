d5991dc6dd5ba2c36591d93ee813791a2f4bcd07
ananyao4 <155084958+ananyao4@users.noreply.github.com>
2024-11-19 15:52:06 +0700

    Update Knn.py

--- Knn.py
+++ Knn.py
@@ -10,15 +10,15 @@ col1, col2, col3 = st.columns(3)
 
 with col1:
    st.header("Versicolor")
-   st.image("./pic/iris1.jpg")
+   st.image("./img/iris1.jpg")
 
 with col2:
    st.header("Verginiga")
-   st.image("./pic/iris2.jpg")
+   st.image("./img/iris2.jpg")
 
 with col3:
    st.header("Setosa")
-   st.image("./pic/iris3.jpg")
+   st.image("./img/iris3.jpg")
 
 html_7 = """
 <div style="background-color:#EC7063;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
@@ -62,7 +62,7 @@ sp_wd = st.number_input("กรุณาเลือกข้อมูล sepalw
 
 if st.button("ทำนายผล"):
     #st.write("ทำนาย")
-   dt = pd.read_csv("./data/iris.csv") 
+   dt = pd.read_csv("./data/iris-3.csv") 
    X = dt.drop('variety', axis=1)
    y = dt.variety   
    Knn_model = KNeighborsClassifier(n_neighbors=3)
@@ -73,10 +73,10 @@ if st.button("ทำนายผล"):
    out=Knn_model.predict(x_input)
 
    if out[0] == 'Setosa':
-    st.image("./pic/iris1.jpg")
+    st.image("./img/iris1.jpg")
    elif out[0] == 'Versicolor':       
-    st.image("./pic/iris2.jpg")
+    st.image("./img/iris2.jpg")
    else:
-    st.image("./pic/iris3.jpg")
+    st.image("./img/iris3.jpg")
 else:
     st.write("ไม่ทำนาย")
